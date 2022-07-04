b3628baeabaf60445f177ddd8c1df7614dfdceb6
Sinan <snnhbyb@gmail.com>
2022-07-04 19:38:29 +0200

    added index and other classes

---
+++ src/main/java/com/example/springboottyhmeleafcrudwebapp/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.example.springboottyhmeleafcrudwebapp.service;
+
+import com.example.springboottyhmeleafcrudwebapp.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+}
