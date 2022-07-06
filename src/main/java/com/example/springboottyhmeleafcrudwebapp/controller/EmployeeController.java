c91c3699eb0ab401559c200f042244f3ee5a7d30
Sinan <snnhbyb@gmail.com>
2022-07-06 17:26:11 +0200

    Add Employee part done

--- src/main/java/com/example/springboottyhmeleafcrudwebapp/controller/EmployeeController.java
+++ src/main/java/com/example/springboottyhmeleafcrudwebapp/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.example.springboottyhmeleafcrudwebapp.controller;
 
+import com.example.springboottyhmeleafcrudwebapp.model.Employee;
 import com.example.springboottyhmeleafcrudwebapp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -15,6 +18,19 @@ public class EmployeeController {
     public String viewHomePage(Model model){
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
+    }
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+
     }
 
 }
